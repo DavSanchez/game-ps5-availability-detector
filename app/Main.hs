de0358fe28dd73afd4cc908c91445a0de170f65d
David Sánchez <david.sanchez.lt@gmail.com>
2021-09-17 00:56:55 +0100

    First complete working version

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-import PS5 (ps5)
+import Colog (cmap, fmtMessage, logTextStdout, usingLoggerT)
+import Control.Monad (forever)
+import PS5 (ps5check)
 
 main :: IO ()
-main = ps5
+main = forever ps5
+  where
+    action = cmap fmtMessage logTextStdout
+    ps5 = usingLoggerT action ps5check
--- src/PS5.hs
+++ src/PS5.hs
@@ -1,22 +1,48 @@
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module PS5 (ps5) where
+module PS5 (ps5check) where
 
+import Colog (Message, WithLog, logError, logInfo, logWarning)
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import Data.Aeson (Value)
 import Data.ByteString (ByteString)
-import Game (isPS5Available, ps5AvailabilityRequest)
-import Network.HTTP.Simple (getResponseBody, getResponseStatusCode, httpJSON)
+import Data.Text (pack)
+import GHC.Conc.IO (threadDelay)
+import Game
+  ( Ps5Availability,
+    isPS5Available,
+    ps5AvailabilityRequest,
+  )
+import Network.HTTP.Simple (Response, getResponseBody, getResponseStatus, httpJSON)
+import Network.HTTP.Types.Status (Status (statusCode, statusMessage), status200)
+import System.IO.Error (catchIOError)
 import Twilio (getTwilioConfig, sendSMSReq)
 
 availabilityMessage :: ByteString
-availabilityMessage = "BRO. LA PS5 ESTÁ DISPONIBLE EN https://www.game.es/ps5-playstation5-reserva. CORRE!"
+availabilityMessage = "\nBRO.\nLA PS5 ESTÁ DISPONIBLE EN https://www.game.es/ps5-playstation5-reserva.\n\nCORRE!"
 
-ps5 :: IO ()
-ps5 = do
-  gameResponse <- httpJSON ps5AvailabilityRequest
-  if isPS5Available gameResponse
-    then do
-      twilioConf <- getTwilioConfig
-      twilioResponse <- httpJSON $ sendSMSReq twilioConf availabilityMessage
-      print (getResponseBody twilioResponse :: Value)
-    else pure ()
+ps5check :: (WithLog env Message m, MonadIO m) => m ()
+ps5check = do
+  gameRes <- httpJSON ps5AvailabilityRequest
+  let status = getResponseStatus gameRes
+  if status == status200
+    then logInfo "[GAME.es] HTTP Response: 200 OK" >> availability gameRes
+    else logError $ "[GAME.es] HTTP Response: " <> (pack . show) (statusCode status) <> " " <> (pack . show) (statusMessage status)
+  liftIO $ threadDelay 60000000 -- 1 per minute
+  liftIO $ pure ()
+
+availability :: (WithLog env Message m, MonadIO m) => Response Ps5Availability -> m ()
+availability g =
+  logInfo ("[GAME.es] Available: " <> (pack . show) a)
+    >> if a
+      then do
+        logWarning "[GAME.es] Looks like PS5 is available! Alerting master..."
+        logInfo "[Twilio] Getting Twilio messaging info"
+        twilioConf <- liftIO getTwilioConfig
+        logInfo "[Twilio] Sending SMS"
+        twilioResponse <- httpJSON $ sendSMSReq twilioConf availabilityMessage
+        logWarning $ "[Twilio] SMS send response: " <> (pack . show) (getResponseBody twilioResponse :: Value)
+      else logInfo "[GAME.es] No luck. PS5 is not available. Retrying in 60 seconds." >> pure ()
+  where
+    a = isPS5Available g
