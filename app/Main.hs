89d4394fc6b600f852984c7d191ced21258625f8
David Sánchez <david.sanchez.lt@gmail.com>
2021-09-17 14:37:17 +0100

    Output to file

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,8 @@
 module Main where
 
-import Colog (LogAction, Message, RichMessage, cmapM, defaultFieldMap, fmtRichMessageDefault, logTextStdout, upgradeMessageAction, usingLoggerT)
+import Colog (LogAction, Message, RichMessage, cmapM, defaultFieldMap, fmtRichMessageDefault, logText, logTextStderr, logTextStdout, upgradeMessageAction, usingLoggerT, withLogTextFile)
 import Control.Monad (forever)
+import Data.Text (Text)
 import PS5 (ps5check)
 
 main :: IO ()
@@ -10,10 +11,11 @@ main = forever ps5
 -- Reference: https://github.com/kowainik/co-log/blob/45d5dd6c33f722628a0f9c10e4297f5e11ede20f/co-log/tutorials/Main.hs
 
 ps5 :: IO ()
-ps5 = usingLoggerT fullMessageAction ps5check
-
-richMessageAction :: LogAction IO (RichMessage IO)
-richMessageAction = cmapM fmtRichMessageDefault logTextStdout
-
-fullMessageAction :: LogAction IO Message
-fullMessageAction = upgradeMessageAction defaultFieldMap richMessageAction
+ps5 = withLogTextFile "game-ps5-availability-detector--logs.log" $ \logTextFile -> do
+  let loggingAction :: LogAction IO Text
+      loggingAction = logTextStdout <> logTextStderr <> logTextFile
+  let richMessageAction :: LogAction IO (RichMessage IO)
+      richMessageAction = cmapM fmtRichMessageDefault loggingAction
+  let fullMessageAction :: LogAction IO Message
+      fullMessageAction = upgradeMessageAction defaultFieldMap richMessageAction
+  usingLoggerT fullMessageAction ps5check
