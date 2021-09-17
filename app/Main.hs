ea7624344a255779e0954c7405b26b75d49a063e
David Sánchez <david.sanchez.lt@gmail.com>
2021-09-17 14:26:54 +0100

    Implement rich logging

--- app/Main.hs
+++ app/Main.hs
@@ -1,11 +1,19 @@
 module Main where
 
-import Colog (cmap, fmtMessage, logTextStdout, usingLoggerT)
+import Colog (LogAction, Message, RichMessage, cmapM, defaultFieldMap, fmtRichMessageDefault, logTextStdout, upgradeMessageAction, usingLoggerT)
 import Control.Monad (forever)
 import PS5 (ps5check)
 
 main :: IO ()
 main = forever ps5
-  where
-    action = cmap fmtMessage logTextStdout
-    ps5 = usingLoggerT action ps5check
+
+-- Reference: https://github.com/kowainik/co-log/blob/45d5dd6c33f722628a0f9c10e4297f5e11ede20f/co-log/tutorials/Main.hs
+
+ps5 :: IO ()
+ps5 = usingLoggerT fullMessageAction ps5check
+
+richMessageAction :: LogAction IO (RichMessage IO)
+richMessageAction = cmapM fmtRichMessageDefault logTextStdout
+
+fullMessageAction :: LogAction IO Message
+fullMessageAction = upgradeMessageAction defaultFieldMap richMessageAction
