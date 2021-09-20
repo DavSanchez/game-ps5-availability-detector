43a45e1e3162d369c2152a4ddf8658d7796efd9b
David Sánchez <david.sanchez.lt@gmail.com>
2021-09-20 18:04:54 +0100

    (WIP) log rotation

--- app/Main.hs
+++ app/Main.hs
@@ -4,7 +4,8 @@
 module Main where
 
 import Colog
-  ( LogAction,
+  ( Limit (LimitTo),
+    LogAction,
     Message,
     MessageField (MessageField),
     RichMessage,
@@ -16,6 +17,7 @@ import Colog
     logTextStdout,
     upgradeMessageAction,
     usingLoggerT,
+    withLogRotation,
     withLogTextFile,
   )
 import Configuration.Dotenv (defaultConfig, loadFile)
@@ -24,6 +26,7 @@ import Data.Text (Text)
 import Data.TypeRepMap (TypeRepMap, delete)
 import PS5 (ps5check)
 import System.Environment (getEnv)
+import System.Directory (removeFile)
 
 main :: IO ()
 main = forever ps5
@@ -34,16 +37,18 @@ ps5 :: IO ()
 ps5 = do
   void $ loadFile defaultConfig
   logFilePath <- getEnv "LOG_FILE_PATH"
-  withLogTextFile logFilePath $ \logTextFile -> do
-    let loggingAction :: LogAction IO Text
-        loggingAction = logTextStdout <> logTextFile
+  withLogRotation (LimitTo 100000) (LimitTo 10) logFilePath removeFile loggingAction (usingLoggerT messageAction ps5check)
+  where
+    loggingAction :: LogAction IO Text
+    loggingAction = logTextStdout <> logTextFile
 
-    let richMessageAction :: LogAction IO (RichMessage IO)
-        richMessageAction = cmapM fmtRichMessageDefault loggingAction
+    richMessageAction :: LogAction IO (RichMessage IO)
+    richMessageAction = cmapM fmtRichMessageDefault loggingAction
 
-    let logFieldMap :: TypeRepMap (MessageField IO)
-        logFieldMap = (delete @"threadId") defaultFieldMap -- Remove thread ID field (not used)
-    let messageAction :: LogAction IO Message
-        messageAction = upgradeMessageAction logFieldMap richMessageAction
+    logFieldMap :: TypeRepMap (MessageField IO) -- Remove thread ID field (not used)
+    logFieldMap = (delete @"threadId") defaultFieldMap
 
-    usingLoggerT messageAction ps5check
+    messageAction :: LogAction IO Message
+    messageAction = upgradeMessageAction logFieldMap richMessageAction
+
+--usingLoggerT messageAction ps5check
